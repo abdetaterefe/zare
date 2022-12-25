f1c03dc15ca4b601cbb5f69ce2b063786955b652
Abdi Terefe <abdetaterefe@gmail.com>
2022-12-25 14:23:30 +0300

    vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import "./app.postcss";
 import App from "./App.svelte";
+import { inject } from '@vercel/analytics';
 
 const app = new App({
   target: document.getElementById("app"),
 });
 
+inject();
 export default app;
